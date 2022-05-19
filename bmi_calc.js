bfda092e9340a57a1a17e2b019294d2d85a3c908
Luke Billings <lukecbillings@gmail.com>
2022-05-19 16:09:52 +0100

    fixed exponent operator in js

--- bmi_calc.js
+++ bmi_calc.js
@@ -4,6 +4,6 @@ button.addEventListener('click', () => {
   const w = parseInt(document.getElementById("weight").value);
   const h = parseInt(document.getElementById("height").value);
   const result = document.getElementById("output");
-  const bmi = (w / h^h);
+  const bmi = (w / h**2);
   result.innerHTML = 'Your dogs bmi is' + " " + bmi;
 });
