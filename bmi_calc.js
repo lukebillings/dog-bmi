07c5fbb85587c9b4189dce8b931076cac33de8f5
Luke Billings <lukecbillings@gmail.com>
2022-05-19 16:50:35 +0100

    changed int to float

--- bmi_calc.js
+++ bmi_calc.js
@@ -2,8 +2,9 @@ let button = document.getElementById("btn");
 
 button.addEventListener('click', () => {
   const w = parseInt(document.getElementById("weight").value);
-  const h = parseInt(document.getElementById("height").value);
+  const h = parseFloat(document.getElementById("height").value);
   const result = document.getElementById("output");
-  const bmi = (w / h**2);
+  const bmi = w / (h**2) ;
   result.innerHTML = 'Your dogs bmi is' + " " + bmi;
+  console.log(w)
 });
