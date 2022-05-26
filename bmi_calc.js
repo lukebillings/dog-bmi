20da432a0a5c7e5644941ce041c86e9e8c54ac5c
Luke Billings <lukecbillings@gmail.com>
2022-05-26 22:08:53 +0100

    got values of weight and height out of selected breed api

--- bmi_calc.js
+++ bmi_calc.js
@@ -1,4 +1,21 @@
-// calculate dog BMI from user imput
+// api url
+
+const breedsURL = "https://api.thedogapi.com/v1/breeds?api_key=e3776e60-173a-4b6b-88cb-8e88737323b1"
+
+// get the dog breeds json into a dropdown list
+
+const selector = document.getElementById('selector')
+
+fetch(breedsURL)
+  .then(response => response.json())
+  .then((data) => {
+      data.forEach((result) => {
+        const dog = `<option value=${result.name}>${result.name} </option>`
+        selector.insertAdjacentHTML("beforeend", dog)
+      })
+  });
+
+// calculate your dog BMI from user imput
 
 let button = document.getElementById("btn");
 
@@ -7,55 +24,29 @@ button.addEventListener('click', () => {
   const h = parseFloat(document.getElementById("height").value);
   const result = document.getElementById("output");
   const bmi = w / (h**2) ;
-  result.innerHTML = 'Your dogs bmi is' + " " + bmi;
-  console.log(w)
+  result.innerHTML = 'Your dog bmi is' + " " + bmi;
 });
 
 
-// api url
+// search for a dog
 
-const breedURL = "https://api.thedogapi.com/v1/breeds?api_key=e3776e60-173a-4b6b-88cb-8e88737323b1"
+const searchForm = document.querySelector("#search-form")
 
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const keyword = document.getElementById("keyword").value;
+  const searchBreed = (keyword) => {
+  fetch(`https://api.thedogapi.com/v1/breeds/search?q=${keyword}`)
+    .then(response => response.json())
+    .then((data) => {
+      const myBreedName = data[0].name;
+      const myBreedWeight = data[0].weight;
+      const myBreedHeight = data[0].height;
+      console.log(myBreedName);
+      console.log(myBreedWeight);
+      console.log(myBreedHeight);
+    })
+  }
+  searchBreed(keyword);
+});
 
-// get request on api gives all the dog breeds
-
-const dog_breeds = document.getElementById("dog_breeds")
-
-fetch(breedURL)
-  .then(response => response.json())
-  .then((data) => {
-      data.forEach((result) => {
-        const dog = `<li>
-          <p>${result.name}</p>
-        </li>`
-        dog_breeds.insertAdjacentHTML("beforeend", dog)
-      })
-  });
-
-
-// get the dog breeds json into a dropdown list
-
-const selector = document.getElementById('selector')
-
-fetch(breedURL)
-  .then(response => response.json())
-  .then((data) => {
-      data.forEach((result) => {
-        const dog = `<option>
-          ${result.name}
-        </option>`
-        selector.insertAdjacentHTML("beforeend", dog)
-      })
-  });
-
-
-// on submit use the value of the dropdown to make get request on api with the value interpolated => print it's bmi onto page
-
-
-
-
-// compared selected dog bmi to your dog bmi, conditional with 3 types of advice
-
-
-
-// display dog image
