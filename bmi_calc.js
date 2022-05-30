7cebae031fd22c4a5f9826d97f889f460f910d69
Luke Billings <lukecbillings@gmail.com>
2022-05-30 12:48:57 +0100

    added conditional advice

--- bmi_calc.js
+++ bmi_calc.js
@@ -1,4 +1,4 @@
-// api url
+// breeds api url
 
 const breedsURL = "https://api.thedogapi.com/v1/breeds?api_key=e3776e60-173a-4b6b-88cb-8e88737323b1"
 
@@ -25,24 +25,20 @@ searchForm.addEventListener("submit", (event) => {
   fetch(`https://api.thedogapi.com/v1/breeds/search?q=${query}`)
     .then(response => response.json())
     .then((data) => {
-      const myBreedName = data[0].name;
+      let myBreedName = data[0].name;
       const myBreedWeight = data[0].weight.metric.split(" ");
       const myBreedHeight = data[0].height.metric.split(" ");
       const myBreedBMILow = (parseInt(myBreedWeight[0]) / ((parseFloat(myBreedHeight[0]))/100)**2).toPrecision(3);
       const myBreedBMIHigh = (parseInt(myBreedWeight[2]) / ((parseFloat(myBreedHeight[2]))/100)**2).toPrecision(3);
-      const imgID = data[0].reference_image_id;
-      console.log(imgID)
+      let imgID = data[0].reference_image_id;
 
       fetch(`https://api.thedogapi.com/v1/images/${imgID}`)
         .then(response => response.json())
         .then((image) => {
           const dogImgURL = image.url;
-          console.log(dogImgURL)
-          const imageResult = document.getElementById("dog-image")
+          const imageResult = document.getElementById("dog-image");
           imageResult.innerHTML = `<img src="${dogImgURL}" alt="" height="200" class="rounded">`
-        })
-
-
+        });
 
       const weight = parseInt(document.getElementById("weight").value);
       const height = parseFloat(document.getElementById("height").value);
@@ -50,10 +46,18 @@ searchForm.addEventListener("submit", (event) => {
       const bmi = (weight / (height**2)).toPrecision(3) ;
       result.innerHTML = `Breed : ${myBreedName} <br />
       Good Range :  ${myBreedBMILow} - ${myBreedBMIHigh} <br />
-      Your BMI : ${bmi}`
-    })
+      Your BMI : ${bmi}`;
+
+      const advice = document.getElementById("advice");
+      if (bmi < myBreedBMILow) {
+        advice.innerHTML = "Your dog is underweight. Please feed it more treets!";
+      } else if (bmi > myBreedBMIHigh) {
+        advice.innerHTML = "You dog is overweight. Please feed it less treets!";
+      } else if (bmi < myBreedBMIHigh && bmi > myBreedBMILow) {
+        advice.innerHTML = "Your dog is healthy! Good job!";
+      }
+
+    });
 });
-
-
 
 
